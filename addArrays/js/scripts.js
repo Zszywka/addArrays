cb4604cd65a680363b84b11e2dfb6f048c4f5b43
Joanna <piotrowskaWIMIIP@gmail.com>
2018-04-06 17:16:41 +0200

    Add variable allNames and function if (allNames.indexOf(newName) === -1) {...}

--- addArrays/js/scripts.js
+++ addArrays/js/scripts.js
@@ -1,1 +1,14 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var newName = 'Marian';
 
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+
+if (allNames.indexOf(newName) === -1) {
+  var addNewNameToAllNames = allNames.push('Marian');
+  console.log(allNames);
+}
+else {
+  console.log("your allNames table has a name from the newName table");
+}
